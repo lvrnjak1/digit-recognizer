ad5ab7c4b282ef6ad2ed2738e41c7187899bcc90
dselimovic <dselimovic1@etf.unsa.ba>
2020-05-30 22:27:08 +0200

    Final

--- app/src/main/java/ba/unsa/etf/digit_recognizer_app/CanvasView.java
+++ app/src/main/java/ba/unsa/etf/digit_recognizer_app/CanvasView.java
@@ -85,7 +85,7 @@ public class CanvasView extends View {
         Bitmap toEncode = canvasBitmap;
         new Thread(() -> {
             ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            toEncode.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+            toEncode.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
             byte[] bytes = byteArrayOutputStream.toByteArray();
             listener.onCanvasGenerated(Base64.encodeToString(bytes, Base64.DEFAULT));
         }).start();
